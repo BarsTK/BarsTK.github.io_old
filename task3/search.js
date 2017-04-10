8d49e5b936582d6e4ab6b1d3ea3b980a9b6152a1
Irbis <UladzimirZinin@gmail.com>
2017-04-10 20:11:05 +0300

    small changes

--- task3/search.js
+++ task3/search.js
@@ -1,7 +1,6 @@
-function binSearch(arrIn, searchNumb) {
-  var arr = arrIn.split(',');
+function binSearch(arr, searchNumb) {
   var len = arr.length;
-  const ascFlag = (+arr[0] < +arr[len - 1]);
+  const ascFlag = (arr[0] < arr[len - 1]);
   const diffIdxPos = 1;
   var leftIdx = 0;
   var rightIdx = len - 1;
@@ -9,14 +8,14 @@ function binSearch(arrIn, searchNumb) {
   var lenHalf;
   var comparingResult;
   
-  if (ascFlag ? (+searchNumb < +arr[0]) && (+searchNumb > +arr[len - diffIdxPos]) : 
-                (+searchNumb > +arr[0]) && (+searchNumb < +arr[len - diffIdxPos])) {
+  if (ascFlag ? (searchNumb < arr[0]) && (searchNumb > arr[len - diffIdxPos]) : 
+                (searchNumb > arr[0]) && (searchNumb < arr[len - diffIdxPos])) {
     return searchedIdx;
   }
   while (leftIdx != rightIdx) {
     lenHalf= Math.floor(len / 2);
-    comparingResult = (ascFlag ? (+searchNumb > +arr[leftIdx + lenHalf - diffIdxPos]) : 
-                                  (+searchNumb < +arr[leftIdx + lenHalf - diffIdxPos]));
+    comparingResult = (ascFlag ? (searchNumb > arr[leftIdx + lenHalf - diffIdxPos]) : 
+                                  (searchNumb < arr[leftIdx + lenHalf - diffIdxPos]));
     if (comparingResult) {
       leftIdx += lenHalf;
     } else {
@@ -24,7 +23,7 @@ function binSearch(arrIn, searchNumb) {
     }
     len = (rightIdx + diffIdxPos) - leftIdx;
   }
-  if (leftIdx == rightIdx && +searchNumb == +arr[leftIdx]) {
+  if (leftIdx == rightIdx && searchNumb == arr[leftIdx]) {
     searchedIdx = leftIdx;
   }
   return searchedIdx;
